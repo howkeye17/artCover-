9e4b4101c83548ec3f401ad744c9899ef0884a32
howkeye17 <howkeye17@gmail.com>
2021-06-03 11:23:54 +0300

    Initial Commit

---
+++ artCover (UITableView+UIImage)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  artCover (UITableView+UIImage)
+//
+//  Created by Valera Vasilevich on 3.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
